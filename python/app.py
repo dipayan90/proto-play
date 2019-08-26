c3bf7338f97af91e10e2a13d8cb6ec5bb8bda8da
dipayan90 <dipayan.chatopadhyay@nordstrom.com>
2019-08-25 22:23:58 -0700

    adding docstrings to python functions

--- python/app.py
+++ python/app.py
@@ -3,15 +3,29 @@ import uuid
 import base64
 
 def serialize(employee: Employee) -> str:
+    """
+    Given an employee object serializes it to a b64 encoded string
+    :param employee: Employee Object
+    :return: b64 encoded string
+    """
     return base64.b64encode(employee.SerializeToString()).decode('utf-8')
 
 def deserialize(employee_str) -> Employee:
+    """
+    Given an base64 encoded string parse and convert to Employee object
+    :param employee_str: base64 encoded string
+    :return: Employee Object
+    """
     decoded = base64.b64decode(employee_str)
     employee_obj = Employee()
     employee_obj.ParseFromString(decoded)
     return employee_obj
 
 def create_employee_object() -> Employee:
+    """
+    Creates a dummy employee object
+    :return:
+    """
     employee = Employee()
     employee.employee_id = str(uuid.uuid4())
     employee.name = "Bob"
@@ -22,11 +36,19 @@ def create_employee_object() -> Employee:
     return employee
 
 def write_to_file():
+    """
+    Writes employee object to file
+    :return:
+    """
     employee: Employee = create_employee_object()
     with open('proto.txt', 'w') as the_file:
         the_file.write(serialize(employee=employee))
 
 def read_from_file():
+    """
+    Reads employee object from file
+    :return:
+    """
     with open('proto.txt', 'r') as the_file:
         employee_obj = deserialize(the_file.readline())
         assert employee_obj.name == "Bob"
